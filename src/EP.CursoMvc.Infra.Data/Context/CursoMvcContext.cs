78b2be5af77f451f806845a402c3a43b722169f0
Thiago Reis <thiagoreis128@gmail.com>
2017-09-19 17:05:29 -0300

    Criando o Contexto

---
+++ src/EP.CursoMvc.Infra.Data/Context/CursoMvcContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EP.CursoMvc.Domain.Entities;
+
+namespace EP.CursoMvc.Infra.Data.Context
+{
+    class CursoMvcContext : DbContext
+    {
+        public CursoMvcContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+
+    }
+}
